faaa9e193997bfbc56f2f8b196c805e6160b1063
AlekWhite <63272278+AlekWhite@users.noreply.github.com>
2025-10-19 22:17:03 -0400

    Update wsgi.py

--- flaskApp/wsgi.py
+++ flaskApp/wsgi.py
@@ -2,4 +2,4 @@ import flask_server
 from flask_server import socketio, app
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port="5100", allow_unsafe_werkzeug=True, debug=True)
+    socketio.run(app, host="0.0.0.0", port="5100", allow_unsafe_werkzeug=True, debug=False)
